9a65105905af016aca2029e8b634e92efb255404
Jakub Berthoty <berthotyms@kros.sk>
2021-07-16 12:37:27 +0200

    Inital commit

---
+++ 03 - App Service/apps/SampleAppVs/SampleAppVs/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace SampleAppVs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public IndexModel(IConfiguration c, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _config = c;
+        }
+
+        public string SomeValue { get => "My setting: " + _config.GetValue<string>("SomeSetting"); }
+
+        public string ConnString { get => "Connection string: " + Environment.GetEnvironmentVariable("SQLCONNSTR_CONNECTION"); }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
