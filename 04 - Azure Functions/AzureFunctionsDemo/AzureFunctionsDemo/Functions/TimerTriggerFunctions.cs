de029e7cd827d7c00defe6b8d398445d0201525e
Jakub Berthoty <berthotyms@kros.sk>
2021-10-01 08:47:44 +0200

    Timer functions initial commit.

---
+++ 04 - Azure Functions/AzureFunctionsDemo/AzureFunctionsDemo/Functions/TimerTriggerFunctions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace AzureFunctionsDemo.Functions
+{
+    public static class TimerTriggerFunctions
+    {
+        [FunctionName(nameof(TimerTriggerFunction))]
+        public static void TimerTriggerFunction([TimerTrigger("*/30 * * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"C# Timer trigger is running {(myTimer.IsPastDue ? "late" : "on time")}.");
+            log.LogInformation($"Next 5 occurences:\n{myTimer.FormatNextOccurrences(5)}");
+        }
+    }
+}
